80546e04d98ee38acb1679bd30b78a9cea442cb2
Billyboii <billyboi456@gmail.com>
2017-12-01 14:32:49 -0500

    Update ReadPass.py

--- ReadPass.py
+++ ReadPass.py
@@ -1,13 +1,23 @@
 import socket
 import os
 import os.path
+import glob
 import no_bytecode
 
 def read_pass(client):
-  file = open("%s.txt" % entry, "r")
-  contents = file.read()
-  file.close
   
-  #decrypt contents variable here
+  for file in glob.glob("*.txt"):
+    dir_contents = os.listdir('.')
+    if dir_contents:
+      #file = file[:-4]
+      print(file)
+      client.send("Enter the name of the entry you would like to read: ")
+      entry = str(client.recv(1024))
+      file = open("%s.txt" % entry, "r")
+      contents = file.read()
+      file.close
   
-  print(contents)
+      #decrypt contents variable here
+      print(contents)
+    else:
+      client.send("There are no saved passwords")
