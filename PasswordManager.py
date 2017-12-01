0a51484dfe7be819c913007579193553ae77a29b
Billyboii <billyboi456@gmail.com>
2017-12-01 11:35:29 -0500

    Update PasswordManager.py

--- PasswordManager.py
+++ PasswordManager.py
@@ -9,13 +9,14 @@ def manage_pass(client):
   tempbool = True
   while tempbool == True:
     client.send("Press 1 to save a username and password\nPress 2 to read a username and password\nPress 3 to go back to the main menu")
-            response = client.recv(1024)
-
-            if response == '1':
-                save_pass(client)
-
-            elif response == '2':
-                read_pass(client)
-
-            elif response == '3':
-                tempbool = False
+    response = client.recv(1024)
+    
+    if response == '1':
+      save_pass(client)
+      
+    elif response == '2':
+    read_pass(client)
+    
+  elif response == '3':
+    tempbool = False
+    
