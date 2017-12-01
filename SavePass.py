3cf5d11fe1e8246ebf7cf61d32803380f4f68425
Billyboii <billyboi456@gmail.com>
2017-12-01 10:46:54 -0500

    Update SavePass.py

--- SavePass.py
+++ SavePass.py
@@ -4,22 +4,15 @@ import os.path
 import no_bytecode
 
 def save_pass(client)
+  client.send("Enter a name for this entry:")
+  entry = str(client.recv(1024))
   client.send("Enter a username:")
-          username = str(client.recv(1024))
-          if username != "":
-               while True:
-                   client.send("Enter a password:")
-                   password = str(client.recv(1024))
-                      if pw_check(password):
-                          client.send("Account created!\n\n")
-                          break
-                      else:
-                          client.send("Please enter a valid password.\n")
-                 break
-             else:
-                  client.send("Please enter a valid username.\n")
-          else:
-              client.send("Username taken, try again.\n")
-  
-      data_file.write(username + ", " + password + "\n")
-      data_file.close()
+  username = str(client.recv(1024))
+  if username != "":
+    while True:
+        client.send("Enter a password:")
+        password = str(client.recv(1024))
+
+file = open("%s.txt" % entry,"w+")		
+file.write(username + ", " + password + "\n")
+file.close()
