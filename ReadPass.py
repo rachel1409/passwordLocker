ecc085775083e227e01f846be4d113c6cc274899
Billyboii <billyboi456@gmail.com>
2017-12-01 11:24:33 -0500

    Create ReadPass.py

---
+++ ReadPass.py
@@ -0,0 +1,10 @@
+import socket
+import os
+import os.path
+import no_bytecode
+
+def read_pass(client)
+  file = open("%s.txt" % entry, "r")
+  contents = file.read()
+  file.close
+  print(contents)
