4ec16c2e4a0fde337b593ffb9a9bd7a48eca5d76
Billyboii <billyboi456@gmail.com>
2017-12-01 14:33:34 -0500

    Update SavePass.py

--- SavePass.py
+++ SavePass.py
@@ -8,13 +8,11 @@ def save_pass(client):
   entry = str(client.recv(1024))
   client.send("Enter a username:")
   username = str(client.recv(1024))
-  if username != "":
-    while True:
-        client.send("Enter a password:")
-        password = str(client.recv(1024))
+  client.send("Enter a password:")
+  password = str(client.recv(1024))
 
-#Convert username and password to encrypted data here and replace variables below with encrypted variables        
+  #Convert username and password to encrypted data here and replace variables below with encrypted variables        
         
-file = open("%s.txt" % entry,"w+")		
-file.write(username + ", " + password + "\n")
-file.close()
+  file = open("%s.txt" % entry,"w+")		
+  file.write(username + ", " + password + "\n")
+  file.close()
