0b68b2ade9300511ad456215e89ec0c92abd8994
Billyboii <billyboi456@gmail.com>
2017-12-04 23:20:45 -0500

    Update ReadPass.py

--- ReadPass.py
+++ ReadPass.py
@@ -2,21 +2,26 @@ import socket
 import os
 import os.path
 import glob
+from rsa import *
+from aes import *
 import no_bytecode
 
-def read_pass(client):
+def read_pass(client, key, clientkey):
+  contents = ""
   dir_contents = os.listdir('.')
   if dir_contents:
     for file in glob.glob("*.txt"):
       file = file[:-4]
       print(file)
-    client.send("Enter the name of the entry you would like to read: ")
-    entry = str(client.recv(1024))
-    file = open("%s.txt" % entry, "r")
-    contents = file.read()
-    file.close
-    
-    #decrypt contents variable here
-    print(contents)
+    client.send(rsaencrypt("Enter the name of the entry you would like to read: ", clientkey))
+    entry = rsadecrypt(client.recv(1024), key)
+
+    with open("%s.csv" % entry, "r") as f:
+      reader = csv.reader()
+      for row in reader:
+        for r in row:
+          contents = contents + aesdecrypt(r, key_gen()) + "\n"
+
+    client.send(rsaencrypt(contents, clientkey))
   else:
-    client.send("There are no saved passwords")
+    client.send(rsaencrypt("There are no saved passwords", clientkey))
