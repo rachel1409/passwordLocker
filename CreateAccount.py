662d94c84c3359409b4156362b82a3f2dfb91247
Billyboii <billyboi456@gmail.com>
2017-12-04 23:18:00 -0500

    Update CreateAccount.py

--- CreateAccount.py
+++ CreateAccount.py
@@ -1,42 +1,63 @@
 import socket
 import os
+import hashlib
+import csv
+from rsa import *
+from aes import *
 import os.path
 from PasswordHandler import *
 import no_bytecode
 
-def create_account(client):
-    file_name = 'data.txt'
+def create_account(client, key, clientkey):
+    file_name = 'data.csv'
+    unames = []
 
-    data_file = open(file_name, 'a+')
-    read_file = open(file_name, 'r')
+    if not os.path.isfile(file_name):
+        w = open(file_name, "w+")
+        w.close()
+    with open(file_name, "r") as read_file:
+        reader = csv.reader(read_file)
+        for row in reader:
+            unames.append(aesdecrypt(row[0], gen_key()))
 
     while True:
-        client.send("Enter a username:")
-        username = str(client.recv(1024))
+        client.send(rsaencrypt("Enter a username:", clientkey))
+        username = rsadecrypt(client.recv(1024), key)
 
-        if username not in read_file.read():
+        if username not in unames:
             if username != "":
                 while True:
-                    client.send("Your password must contain:\n-A minimum of 24 character\n-At least 1 uppercase letter, 1 lowercase letter, 1 number, and 1 symbol.\n")
-                    client.send("Enter a password:")
-                    password = str(client.recv(1024))
+                    #client.send(rsaencrypt("Password must contain a minimum of 24 character.\n", clientkey))
+                    #client.send(rsaencrypt("Password must contain:\n", clientkey))
+                    #client.send(rsaencrypt("-1 uppercase letter\n-1 lowercase letter\n-1 number\n-1 symbol\n", clientkey))
+                    client.send(rsaencrypt("Enter a password:", clientkey))
+                    password = rsadecrypt(client.recv(1024), key)
 
                     if pw_check(password):
-                        client.send("Account created!\n\n")
+                        client.send(rsaencrypt("Account created!\n", clientkey))
                         break
                     else:
-                        client.send("Please enter a valid password.\n")
+                        client.send(rsaencrypt("Please enter a valid password.\n", clientkey))
                 break
             else:
-                client.send("Please enter a valid username.\n")
+                client.send(rsaencrypt("Please enter a valid username.\n", clientkey))
         else:
-            client.send("Username taken, try again.\n")
+            client.send(rsaencrypt("Username taken, try again.\n", clientkey))
 
            
-    #get hash of password (and maybe username) store those variables instead of storing the password itself
-    
+    #get hash of password and store those variables instead of storing the password itself
+    psSalt = os.urandom(256)
+    psHash = hashlib.sha256()
+    psHash.update(password+psSalt.encode('base-64'))
+
     directory = username
     if not os.path.exists(directory): #creates a directory named after the user that will act as the storage location for their passwords
         os.makedirs(directory)
-    data_file.write(username + ", " + password + "\n")
-    data_file.close()
+    with open(file_name, "a") as file_name:
+        writer = csv.writer(file_name)
+        writer.writerow([aesencrypt(username, gen_key(), gen_iv()), aesencrypt(psHash.hexdigest(), gen_key(), gen_iv()), aesencrypt(psSalt.encode('base-64'), gen_key(), gen_iv())])
+
+
+
+
+
