0b33d420675787ab8cda7dfab911febc0993e2f3
Billyboii <billyboi456@gmail.com>
2017-12-04 23:16:47 -0500

    Add files via upload

--- client.py
+++ client.py
@@ -1,6 +1,8 @@
 import socket
 import sys
+from rsa import *
 import no_bytecode
+import threading
 
 def connect(server):
     host = "127.0.0.1"
@@ -8,22 +10,45 @@ def connect(server):
     server.connect((host, port))
     server.sendall("Client connecting")
 
+def recv():
+    response = rsadecrypt(server.recv(1024), key)
+    if response == "Goodbye":
+        server.close()
+        sys.exit()
+    else:
+        while True:
+            print response
+            data = raw_input()
+            
+            if data:
+                server.sendall(rsaencrypt(data, serverkey))
+                break
 
 if __name__ == '__main__':
     server = socket.socket()
     connect(server)
 
-    while True:
-        response = server.recv(1024)
-        if response == "Goodbye":
+    pubkeyfile = 'client.pem'
+    key = gen_privkey()
+    save_pubkey(pubkeyfile, gen_pubkey(key))
+    serverkey = get_pubkey(str(server.recv(1024)))
+    server.sendall(pubkeyfile)
 
+    while True:
+        #switch this on and removing everything below it will fix the bug where we cant send multiple messages to the client.
+        #When it errors out, it spams a ton of text for about a minute so lets debug the rest of the program before we tackle the threading issue
+
+        #threading.Thread(target=recv).start() 
+        
+        response = rsadecrypt(server.recv(1024), key)
+        if response == "Goodbye":
             server.close()
             sys.exit()
         else:
             while True:
                 print response
                 data = raw_input()
-
+                
                 if data:
-                    server.sendall(data)
+                    server.sendall(rsaencrypt(data, serverkey))
                     break
