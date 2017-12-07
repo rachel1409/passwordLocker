828d76420e245112d1131d1a70387819ef9efa99
Billyboii <billyboi456@gmail.com>
2017-12-06 20:28:28 -0500

    Update PasswordManager.py

--- PasswordManager.py
+++ PasswordManager.py
@@ -1,24 +1,33 @@
 import socket
 import os
+import sys
 import os.path
 import no_bytecode
 from SavePass import *
-from rsa import *
+from PLcrypto import *
 from ReadPass import *
 from ClearScreen import *
 
-def manage_pass(client, key, clientkey):
+def manage_pass(client, key, clientkey, aeskey):
   tempbool = True
+  message = ""
   while tempbool == True:
-    client.send(rsaencrypt("Press:\n- 1 to save username/password\n- 2 to read username/password\n- 3 for main menu", clientkey))
-    response = rsadecrypt(client.recv(1024), key)
+    client.send(PLencrypt(message+"Press:\n- 1 to save username/password\n- 2 to read username/password\n- 3 for main menu", key, aeskey))
+    response = PLdecrypt(client.recv(1024), clientkey, aeskey)
+    if not response:
+      client.shutdown(socket.SHUT_RDWR)
+      client.close()
+      sys.exit(1)
     
     if response == '1':
-      save_pass(client, key, clientkey)
+      clearscrn()
+      save_pass(client, key, clientkey, aeskey)
+      clearscrn()
+      message = "Password Saved!\n"
       
     elif response == '2':
-      #clearscrn()
-      read_pass(client, key, clientkey)
+      clearscrn()
+      message = read_pass(client, key, clientkey, aeskey)
     
     elif response == '3':
       tempbool = False
