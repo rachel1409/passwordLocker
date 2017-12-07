37516b155fca9cea521a2c087d1647ced2a064d9
Billyboii <billyboi456@gmail.com>
2017-12-06 19:22:38 -0500

    Update client.py

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 import socket
 import sys
 from rsa import *
+from PLcrypto import *
 import no_bytecode
 import threading
 
@@ -24,6 +25,14 @@ def recv():
                 server.sendall(rsaencrypt(data, serverkey))
                 break
 
+def checkVerification(server, message):
+    if not message:
+        server.shutdown(socket.SHUT_RDWR)
+        server.close()
+        sys.exit(1)
+    else:
+        return message
+
 if __name__ == '__main__':
     server = socket.socket()
     connect(server)
@@ -33,22 +42,29 @@ if __name__ == '__main__':
     save_pubkey(pubkeyfile, gen_pubkey(key))
     serverkey = get_pubkey(str(server.recv(1024)))
     server.sendall(pubkeyfile)
+    aeskey = rsadecrypt(server.recv(1024), key)
+    if not aeskey:
+        sys.exit(1)
+    if checkVerification(server, PLdecrypt(server.recv(1024), serverkey, aeskey)):
+        server.sendall(PLencrypt("key received", key, aeskey))
 
-    while True:
-        #switch this on and removing everything below it will fix the bug where we cant send multiple messages to the client.
-        #When it errors out, it spams a ton of text for about a minute so lets debug the rest of the program before we tackle the threading issue
+        while True:
+            #switch this on and removing everything below it will fix the bug where we cant send multiple messages to the client.
+            #When it errors out, it spams a ton of text for about a minute so lets debug the rest of the program before we tackle the threading issue
 
-        #threading.Thread(target=recv).start() 
-        
-        response = rsadecrypt(server.recv(1024), key)
-        if response == "Goodbye":
-            server.close()
-            sys.exit()
-        else:
-            while True:
-                print response
-                data = raw_input()
-                
-                if data:
-                    server.sendall(rsaencrypt(data, serverkey))
-                    break
+            #threading.Thread(target=recv).start() 
+            
+            response = checkVerification(server, PLdecrypt(server.recv(1024), serverkey, aeskey))
+
+            if response == "Goodbye":
+                server.shutdown(socket.SHUT_RDWR)
+                server.close()
+                sys.exit()
+            else:
+                while True:
+                    print response
+                    data = raw_input()
+                    
+                    if data:
+                        server.sendall(PLencrypt(data, key, aeskey))
+                        break
