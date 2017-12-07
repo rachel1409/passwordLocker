581dab8e18f19ea59d4ee0899fd83f9f81db3afb
Billyboii <billyboi456@gmail.com>
2017-12-07 11:46:36 -0500

    Update SavePass.py

--- SavePass.py
+++ SavePass.py
@@ -18,7 +18,7 @@ def checkVerification(client, message):
 def save_pass(client, key, clientkey, aeskey):
   client.send(PLencrypt("Enter a name for this entry:", key, aeskey))
   entry = checkVerification(client, PLdecrypt(client.recv(1024), clientkey, aeskey))
-  if not os.path.isfile("%s.csv" % entry)"
+  if not os.path.isfile("%s.csv" % (entry))"
     client.send(PLencrypt("Enter a username:", key, aeskey))
     username = checkVerification(client, PLdecrypt(client.recv(1024), clientkey, aeskey))
     client.send(PLencrypt("Enter a password:", key, aeskey))
