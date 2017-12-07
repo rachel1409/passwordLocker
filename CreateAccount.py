809754ef825dd22b530f7e601082aebd93637402
Billyboii <billyboi456@gmail.com>
2017-12-06 19:23:38 -0500

    Update CreateAccount.py

--- CreateAccount.py
+++ CreateAccount.py
@@ -1,48 +1,56 @@
 import socket
 import os
+import sys
 import hashlib
 import csv
-from rsa import *
+from PLcrypto import *
 from aes import *
 import os.path
 from PasswordHandler import *
 import no_bytecode
 
-def create_account(client, key, clientkey):
+def checkVerification(client, message):
+    if not message:
+        client.shutdown(socket.SHUT_RDWR)
+        client.close()
+        sys.exit(1)
+    else:
+        return message
+
+def create_account(client, key, clientkey, aeskey):
     file_name = 'data.csv'
     unames = []
-
+    umessage = ""
+    pmessage = ""
+    retval = "The account could not be created\n"
     if not os.path.isfile(file_name):
         w = open(file_name, "w+")
         w.close()
     with open(file_name, "r") as read_file:
         reader = csv.reader(read_file)
         for row in reader:
-            unames.append(aesdecrypt(row[0], gen_key()))
+            unames.append(aesdecrypt(row[0], enckey()))
 
     while True:
-        client.send(rsaencrypt("Enter a username:", clientkey))
-        username = rsadecrypt(client.recv(1024), key)
+        client.send(PLencrypt(umessage+"Enter a username:", key, aeskey))
+        username = checkVerification(client, PLdecrypt(client.recv(1024), clientkey, aeskey))
 
         if username not in unames:
             if username != "":
                 while True:
-                    #client.send(rsaencrypt("Password must contain a minimum of 24 character.\n", clientkey))
-                    #client.send(rsaencrypt("Password must contain:\n", clientkey))
-                    #client.send(rsaencrypt("-1 uppercase letter\n-1 lowercase letter\n-1 number\n-1 symbol\n", clientkey))
-                    client.send(rsaencrypt("Enter a password:", clientkey))
-                    password = rsadecrypt(client.recv(1024), key)
+                    client.send(PLencrypt(pmessage+"Password must contain:\n- a minimum of 24 characters\n-1 uppercase letter\n-1 lowercase letter\n-1number\n-1symbol\nEnter a password:", key, aeskey))
+                    password = checkVerification(client, PLdecrypt(client.recv(1024), clientkey, aeskey))
 
                     if pw_check(password):
-                        client.send(rsaencrypt("Account created!\n", clientkey))
+                        retval = "Account created!\n"
                         break
                     else:
-                        client.send(rsaencrypt("Please enter a valid password.\n", clientkey))
+                        pmessage = "Please enter a valid password.\n"
                 break
             else:
-                client.send(rsaencrypt("Please enter a valid username.\n", clientkey))
+                pmessage = "Please enter a valid username.\n"
         else:
-            client.send(rsaencrypt("Username taken, try again.\n", clientkey))
+            pmessage = "Username taken, try again.\n"
 
            
     #get hash of password and store those variables instead of storing the password itself
@@ -53,9 +61,10 @@ def create_account(client, key, clientkey):
     directory = username
     if not os.path.exists(directory): #creates a directory named after the user that will act as the storage location for their passwords
         os.makedirs(directory)
-    with open(file_name, "a") as file_name:
-        writer = csv.writer(file_name)
-        writer.writerow([aesencrypt(username, gen_key(), gen_iv()), aesencrypt(psHash.hexdigest(), gen_key(), gen_iv()), aesencrypt(psSalt.encode('base-64'), gen_key(), gen_iv())])
+    with open(file_name, "a") as f:
+        writer = csv.writer(f)
+        writer.writerow([aesencrypt(username, enckey(), gen_iv()), aesencrypt(psHash.hexdigest(), enckey(), gen_iv()), aesencrypt(psSalt.encode('base-64'), enckey(), gen_iv())])
+    return retval
 
 
 
