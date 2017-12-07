7409b13a07091ede98444d97b2a0c174510144dc
Billyboii <billyboi456@gmail.com>
2017-12-07 11:52:51 -0500

    Update SavePass.py

--- SavePass.py
+++ SavePass.py
@@ -27,7 +27,7 @@ def save_pass(client, key, clientkey, aeskey):
     with open("%s.csv" % entry,"w+") as f:
     writer = csv.writer(f)
     writer.writerow([aesencrypt(username, enckey("../"), gen_iv()), aesencrypt(password, enckey("../"), gen_iv())])
-  elif:
+  else:
     clearscrn()
     return "An entry with this name already exists.\n"    
 
