62d98a5eb5e9e14fe396deda868f4bfd67b95a53
Billyboii <billyboi456@gmail.com>
2017-12-07 11:53:37 -0500

    Update SavePass.py

--- SavePass.py
+++ SavePass.py
@@ -25,8 +25,8 @@ def save_pass(client, key, clientkey, aeskey):
     password = checkVerification(client, PLdecrypt(client.recv(1024), clientkey, aeskey))
     
     with open("%s.csv" % entry,"w+") as f:
-    writer = csv.writer(f)
-    writer.writerow([aesencrypt(username, enckey("../"), gen_iv()), aesencrypt(password, enckey("../"), gen_iv())])
+       writer = csv.writer(f)
+      writer.writerow([aesencrypt(username, enckey("../"), gen_iv()), aesencrypt(password, enckey("../"), gen_iv())])
   else:
     clearscrn()
     return "An entry with this name already exists.\n"    
