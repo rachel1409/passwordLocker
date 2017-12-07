aa80de792c98781f0feb21382a111c5f89144b22
Billyboii <billyboi456@gmail.com>
2017-12-07 12:06:40 -0500

    Update Program.py

--- Program.py
+++ Program.py
@@ -3,8 +3,10 @@ import server
 import os
 import time
 import no_bytecode
+from ClearScreen import *
 
 if __name__ == '__main__':
+    clearscrn()
     os.system('python -B server.py &')
     time.sleep(2)
     os.system('python -B client.py')
