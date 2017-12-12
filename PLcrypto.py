8642568e3e43e38956da31c074ee90e7d509e6f1
annemariebogar <abogar@fordham.edu>
2017-12-11 19:22:43 -0500

    Update PLcrypto.py

--- PLcrypto.py
+++ PLcrypto.py
@@ -3,19 +3,22 @@ from rsa import *
 import socket
 import base64
 
+# digital signature and AES encryption
 def PLencrypt(message, privkey, aeskey):
-	aeskey = base64.b64decode(aeskey)
-	signtext = sign(message, privkey)
-	return aesencrypt(signtext, aeskey, gen_iv())
+	aeskey = base64.b64decode(aeskey) # decode aes key
+	signtext = sign(message, privkey) # sign message with RSA private key
+	return aesencrypt(signtext, aeskey, gen_iv()) #encrypt message and signature with AES-256 mode CBC and new IV
 
+# AES decryption and signature verification
 def PLdecrypt(ciphertext, pubkey, aeskey):
-	aeskey = base64.b64decode(aeskey)
-	signtext = aesdecrypt(ciphertext, aeskey)
-	if not verify(signtext[172:], signtext[:172], pubkey):
+	aeskey = base64.b64decode(aeskey) # decode aes key
+	signtext = aesdecrypt(ciphertext, aeskey) # decrypt message with AES-256 mode CBC
+	if not verify(signtext[172:], signtext[:172], pubkey): # check digital signature verification
 		print "Verification Failed"
 		return False
 	else:
 		return signtext[172:]
 
+# retrieve AES key known only to the server
 def enckey(prefix=""):
 	return get_key(prefix+"aes.txt")
